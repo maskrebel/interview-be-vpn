e21aa0913b07633a9569af14267a613501f28d07
maskrebel <33626739+maskrebel@users.noreply.github.com>
2024-11-25 09:19:21 +0700

    feat: set ACCESS_TOKEN_DOMAIN COOKIES

--- smp_abc/smp_abc/settings.py
+++ smp_abc/smp_abc/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-bk%2tm)v+j6a=zn#83=wh&)89((txnly5dubnka@$5=^bst#&t
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['smp.abc.sch.id', 'localhost', '127.0.0.1']
 
 
 # Application definition
